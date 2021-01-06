2b1e73ddc10ecf47ebdc0655ab7e0acbbb656e7a
smishra11 <msubhasish11@gmail.com>
2021-01-06 19:18:08 +0530

    Added load more button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,21 @@ import Haker from './components/Haker';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(10);
+  const [updatedData, setUpdatedData] = useState([]);
+  const [updatedPromises, setUpdatedPromises] = useState([]);
+
+  const loadingData = (allData, start, end) => {
+    const promises = allData
+      .slice(start, end)
+      .map((id) =>
+        fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+        ).then((res) => res.json())
+      );
+    return promises;
+  };
 
   useEffect(() => {
     async function getTopStories() {
@@ -15,22 +30,22 @@ const App = () => {
         }
         const allData = await response.json();
         console.log(allData);
-        const promises = allData
-          .slice(0, 10)
-          .map((id) =>
-            fetch(
-              `https://hacker-news.firebaseio.com/v0/item/${id}.json`
-            ).then((res) => res.json())
-          );
+        const promises = await loadingData(allData, start, end);
+        console.log(promises);
         const result = await Promise.all(promises);
         setPosts(result);
-        console.log(result);
+        // console.log(result);
       } catch (err) {
         console.error(err);
       }
     }
     getTopStories();
   }, []);
+
+  const handleClick = () => {
+    console.log('btn clicked');
+    loadingData();
+  };
 
   return (
     <div className="container">
@@ -39,7 +54,18 @@ const App = () => {
           <b>Haker News Reader</b>
         </span>
       </nav>
-      <Haker data={posts} />
+      {posts.length === 0 ? (
+        <div className=" mt-4 d-flex justify-content-center">
+          <div>
+            <div className="spinner-border text-info ml-3" role="status"></div>
+            <div>
+              <b>Loading...</b>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Haker data={posts} handleClick={handleClick} />
+      )}
     </div>
   );
 };
--- src/components/Haker.js
+++ src/components/Haker.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
-const Haker = ({ data }) => {
-  if (data.length === 0) {
-    <div class="spinner-border text-info" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>;
-  }
+const Haker = ({ data, handleClick }) => {
   return (
     <div className="list-group mt-3">
       {data.map((news_item) => (
@@ -13,15 +8,29 @@ const Haker = ({ data }) => {
           className="list-group-item list-group-item-action flex-column align-items-start haker_item"
           key={news_item.id}
         >
-          <a href={news_item.url}>
-            <p className="m-0">{news_item.title}</p>
-          </a>
-          <small className="text-muted">
+          <p className="mb-0">
+            <a
+              href={news_item.url}
+              style={{
+                textDecoration: 'none',
+                color: 'black',
+                fontSize: '14px',
+              }}
+            >
+              {news_item.title}
+            </a>
+          </p>
+          <small className="text-muted" style={{ fontSize: '11px' }}>
             {news_item.score} points by {news_item.by} | {news_item.descendants}{' '}
             comments
           </small>
         </div>
       ))}
+      <div className="d-flex justify-content-center mt-3">
+        <button className="btn btn-outline-info btn-sm" onClick={handleClick}>
+          Click to load more
+        </button>
+      </div>
     </div>
   );
 };
